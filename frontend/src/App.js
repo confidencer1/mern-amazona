e334aaec8f55ac4b52c0e7194f5c0f35bcc4bb6d
confidencer1 <confidencesmithjason0219@gmail.com>
2023-10-23 18:28:14 +0900

    6-react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <a href="#">amazona</a>
-      </header>
-      <main>list products</main>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <a href="/">amazona</a>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="/product/:slug" element={<ProductScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
